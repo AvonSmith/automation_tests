d9751318294c184dafaee2c284c3ba6c9e97135e
avon_smith <kuznechenkov.ivan00@gmail.com>
2023-01-13 17:10:39 +0100

    Autotest: "Checkbox test"

--- src/main/java/pages/ElementsPage.java
+++ src/main/java/pages/ElementsPage.java
@@ -1,8 +1,12 @@
 package pages;
 
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ElementsPage extends BasePage {
 
@@ -12,10 +16,93 @@ public class ElementsPage extends BasePage {
 
     @FindBy(xpath = "//input[@id='userName']")
     private WebElement fullName;
+
     @FindBy(xpath = "//input[@id='userEmail']")
     private WebElement email;
+
     @FindBy(xpath = "//textarea[@id='currentAddress']")
     private WebElement currentAddress;
+
+    @FindBy(xpath = "//textarea[@id='permanentAddress']")
+    private WebElement permanentAddress;
+
+    @FindBy(xpath = "//button[@id='submit']")
+    private WebElement submitButton;
+
+    @FindBy(xpath = "//p[@id='name']")
+    private WebElement outputFullName;
+
+    @FindBy(xpath = "//p[@id='email']")
+    private WebElement outputEmail;
+
+    @FindBy(xpath = "//p[@id='currentAddress']")
+    private WebElement outputCurrentAddress;
+
+    @FindBy(xpath = "//p[@id='permanentAddress']")
+    private WebElement outputPermanentAddress;
+
+    @FindBy(xpath = "//*[text()='Elements']")
+    private WebElement elements;
+
+    @FindBy(xpath = "//span[text()='Text Box']")
+    private WebElement textBox;
+
+    @FindBy(xpath = "//span[text()='Check Box'] ")
+    private WebElement checkBox;
+
+    @FindBy(xpath = "//span[@class='rct-checkbox']")
+    private List<WebElement> checkboxList;
+
+    @FindBy(xpath = "//button[@aria-label='Expand all']")
+    private WebElement expandList;
+
+    @FindBy(xpath = "//*[contains(@class, 'rct-icon rct-icon-')][contains(@class, 'check')] ")
+    private List<WebElement> checkBoxState;
+
+    public ElementsPage enter(WebElement element, String text) {
+        element.clear();
+        element.sendKeys(text);
+        return this;
+    }
+
+    public void checkAllCheckBoxes() {
+        expandList.click();
+        for (int i = checkboxList.size() - 1; i >= 0; i--) {
+            if (checkBoxState.get(i).getAttribute("class").contains("uncheck"))
+            checkboxList.get(i).click();
+        }
+    }
+
+    public Boolean checkboxState() {
+        boolean result = false;
+        for (int i = 0; i < checkboxList.size(); i++) {
+            if (checkBoxState.get(i).getAttribute("class").equals("rct-icon rct-icon-check"))
+                result = true;
+            else {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public String getOutput(WebElement element) {
+        return element.getText().split(":")[1];
+    }
+
+    public void openTextBox() {
+        elements.click();
+        textBox.click();
+    }
+
+    public void openCheckbox() {
+        elements.click();
+        checkBox.click();
+    }
+
+    public void submit() {
+        submitButton.click();
+    }
 
     public WebElement email() {
         return email;
@@ -33,50 +120,6 @@ public class ElementsPage extends BasePage {
         return fullName;
     }
 
-    @FindBy(xpath = "//textarea[@id='permanentAddress']")
-    private WebElement permanentAddress;
-    @FindBy(xpath = "//button[@id='submit']")
-    private WebElement submitButton;
-    @FindBy(xpath = "//p[@id='name']")
-    private WebElement outputFullName;
-    @FindBy(xpath = "//p[@id='email']")
-    private WebElement outputEmail;
-    @FindBy(xpath = "//p[@id='currentAddress']")
-    private WebElement outputCurrentAddress;
-    @FindBy(xpath = "//p[@id='permanentAddress']")
-    private WebElement outputPermanentAddress;
-    @FindBy(xpath = "//*[@id='app']/div/div/div/div/div[1]/div/div[2]")
-    private WebElement elements;
-    @FindBy(xpath = "//span[text()='Text Box']")
-    private WebElement textBox;
-
-    public ElementsPage enter(WebElement element, String text) {
-        element.clear();
-//        element.click();
-        element.sendKeys(text);
-        return this;
-    }
-
-    public void enterFullName(String text) {
-        fullName.click();
-        fullName.sendKeys(text);
-    }
-
-    public void enterEmail(String text) {
-        email.click();
-        email.sendKeys(text);
-    }
-
-    public void enterCurrentAddress(String text) {
-        currentAddress.click();
-        currentAddress.sendKeys(text);
-    }
-
-    public void enterPermanentAddress(String text) {
-        permanentAddress.click();
-        permanentAddress.sendKeys(text);
-    }
-
     public WebElement getOutputFullName() {
         return outputFullName;
     }
@@ -91,18 +134,5 @@ public class ElementsPage extends BasePage {
 
     public WebElement getOutputPermanentAddress() {
         return outputPermanentAddress;
-    }
-
-    public String getOutput(WebElement element) {
-        return element.getText().split(":")[1];
-    }
-
-    public void startTest() {
-        elements.click();
-        textBox.click();
-    }
-
-    public void submit() {
-        submitButton.click();
     }
 }
--- src/test/java/tests/ElementsTest.java
+++ src/test/java/tests/ElementsTest.java
@@ -2,43 +2,56 @@ package tests;
 
 import data_provider.DataProviders;
 import org.testng.Assert;
-import org.testng.annotations.BeforeTest;
+import org.testng.annotations.BeforeGroups;
 import org.testng.annotations.Test;
 
 import static constants.Constants.Urls.MAIN_URL;
 
-public class ElementsTest extends BaseTest{
-    @BeforeTest
-    public void openTestPage() {
+public class    ElementsTest extends BaseTest{
+
+    @BeforeGroups(groups = "elements")
+    public void elements() {
         page.open(frameworkProperties.getProperty(MAIN_URL));
-        elementsPage.startTest();
+        elementsPage.openTextBox();
     }
 
-    @Test(dataProvider = "params", dataProviderClass = DataProviders.class)
+    @Test(groups = "elements", dataProvider = "params", dataProviderClass = DataProviders.class)
     public void fullNameField(String params) {
         elementsPage.enter(elementsPage.fullName(), params)
                     .submit();
         Assert.assertEquals(elementsPage.getOutput(elementsPage.getOutputFullName()), params);
     }
 
-    @Test(dataProvider = "email", dataProviderClass = DataProviders.class)
+    @Test(groups = "elements", dataProvider = "email", dataProviderClass = DataProviders.class)
     public void emailField(String email) {
         elementsPage.enter(elementsPage.email(), email)
                     .submit();
         Assert.assertEquals(elementsPage.getOutput(elementsPage.getOutputEmail()), email);
     }
 
-    @Test(dataProvider = "address", dataProviderClass = DataProviders.class)
+    @Test(groups = "elements", dataProvider = "address", dataProviderClass = DataProviders.class)
     public void currentAddressTextArea(String address) {
         elementsPage.enter(elementsPage.currentAddress(), address)
                     .submit();
         Assert.assertEquals(elementsPage.getOutput(elementsPage.getOutputCurrentAddress()), address);
     }
 
-    @Test(dataProvider = "address", dataProviderClass = DataProviders.class)
+    @Test(groups = "elements", dataProvider = "address", dataProviderClass = DataProviders.class)
     public void permananetAddressTextArea(String address) {
         elementsPage.enter(elementsPage.permanentAddress(), address)
                     .submit();
         Assert.assertEquals(elementsPage.getOutput(elementsPage.getOutputPermanentAddress()), address);
+    }
+
+    @BeforeGroups(groups = "checkbox")
+    public void checkbox() {
+        page.open(frameworkProperties.getProperty(MAIN_URL));
+        elementsPage.openCheckbox();
+    }
+
+    @Test(groups = "checkbox")
+    public void checkAllCheckBoxes() {
+        elementsPage.checkAllCheckBoxes();
+        Assert.assertTrue(elementsPage.checkboxState());
     }
 }
